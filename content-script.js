27e21ae59f63f23ec7ada45d865fbd1e9304bfd1
monsieurvor <yvardankers@gmail.com>
2023-02-19 02:42:20 +0100

    test1

--- content-script.js
+++ content-script.js
@@ -1,7 +1,10 @@
 var muted = false;
 var volumeState = 0;
 var actualVolume = 0;
-var mouseX;
+var injectedItems = {};
+var lastTime = -1;
+var lastSpeed = 0;
+var setSpeed = 1;
 // Using localStorage as a fallback for chrome.storage.local
 var keybinds = JSON.parse(localStorage.getItem("yt-keybinds"));
 chrome.storage.local.get(["keybinds"])
@@ -73,10 +76,8 @@ document.addEventListener("keydown", (data) => {
 });
 
 const getCurrentId = () => {
-  const videoEle = document.querySelector(
-    "#shorts-player > div.html5-video-container > video"
-  );
-  if (videoEle && videoEle.closest("ytd-reel-video-renderer")) return videoEle.closest("ytd-reel-video-renderer").id;
+  const videoEle = document.querySelector("ytd-reel-video-renderer[is-active='']");
+  if (videoEle) return videoEle.id;
   return null;
 };
 
@@ -96,7 +97,8 @@ const getVolumeContainer = (id) =>
   );
 
 const getNextButton = () =>
-  document.querySelector('button.yt-spec-button-shape-next[aria-label="Next video"]');
+  document.querySelector('button.yt-spec-button-shape-next[aria-label="Next video"]'
+  );
 
 const setTimer = (currTime, duration) => {
   const id = getCurrentId();
@@ -105,6 +107,158 @@ const setTimer = (currTime, duration) => {
     `ytTimer${id}`
   ).innerText = `${currTime}/${duration}s`;
   return true
+};
+
+const setActionList = (currentId, actionList) => {
+  const betterYTContainer = document.createElement("div");
+  betterYTContainer.id = "betterYT-container";
+  betterYTContainer.setAttribute("class", "button-container style-scope ytd-reel-player-overlay-renderer");
+
+  const ytdButtonRenderer = document.createElement("div");
+  ytdButtonRenderer.setAttribute("class", "betterYT-renderer style-scope ytd-reel-player-overlay-renderer");
+
+  const ytButtonShape = document.createElement("div");
+  ytButtonShape.setAttribute("class", "betterYT-button-shape");
+
+  const ytLabel = document.createElement("label");
+  ytLabel.setAttribute("class", "yt-spec-button-shape-with-label");
+
+  const ytButton = document.createElement("button");      
+  ytButton.setAttribute("class", "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-l yt-spec-button-shape-next--icon-button ");
+  // Playback Rate
+  var para0 = document.createElement("p");
+  para0.classList.add("betterYT");
+  para0.id = `ytPlayback${currentId}`;
+
+  // Timer
+  const ytTimer = document.createElement("div");
+  ytTimer.classList.add("yt-spec-button-shape-with-label__label");
+  var span1 = document.createElement("span");
+  span1.setAttribute("class", "yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--text-alignment-center yt-core-attributed-string--word-wrapping");
+  span1.id = `ytTimer${currentId}`;
+  span1.setAttribute("role", "text");
+  ytTimer.appendChild(span1);
+
+  // Match YT's HTML structure
+  ytButton.appendChild(para0);
+  ytLabel.appendChild(ytButton);
+  ytLabel.appendChild(ytTimer);
+  ytButtonShape.appendChild(ytLabel);
+  ytdButtonRenderer.appendChild(ytButtonShape);
+  betterYTContainer.appendChild(ytdButtonRenderer);
+
+  actionList.insertBefore(betterYTContainer, actionList.children[1]);
+
+  // Autoplay Switch
+  const switchContainer = document.createElement("div");
+  const autoplaySwitch = document.createElement("label");
+  autoplaySwitch.classList.add("autoplay-switch");
+  var checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.id = `autoplay-checkbox${currentId}`;
+  checkBox.checked = autoplayEnabled;
+  var autoplaySpan = document.createElement("span");
+  autoplaySpan.classList.add("autoplay-slider");
+  autoplaySwitch.append(checkBox, autoplaySpan);
+  switchContainer.appendChild(autoplaySwitch);
+
+  actionList.insertBefore(switchContainer, actionList.children[1]);
+  
+  const autoplayTitle = document.createElement("div");
+  autoplayTitle.classList.add("yt-spec-button-shape-with-label__label");
+  var span2 = document.createElement("span");
+  span2.setAttribute("class", "betterYT-auto yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--text-alignment-center");
+  span2.setAttribute("role", "text");
+  span2.textContent = "Autoplay";
+  autoplayTitle.appendChild(span2);
+
+  actionList.insertBefore(autoplayTitle, actionList.children[2]);
+  injectedItems.add(currentId);
+
+  return injectedSuccess
+};
+
+const setActionListFuncs = (ytShorts) => {
+  
+  ytShorts.playbackRate = setSpeed;
+  setPlaybackRate(setSpeed);
+  injectedSuccess = setTimer(currTime || 0, Math.round(ytShorts.duration || 0));
+
+  betterYTContainer.addEventListener("click",() => {
+    ytShorts.playbackRate = 1;
+    setSpeed = ytShorts.playbackRate;
+  });
+
+  checkBox.addEventListener('change', () => {
+    if (checkBox.checked) {
+      localStorage.setItem("yt-autoplay", "true");
+      ytShorts.loop = false;
+    } else {
+      localStorage.setItem("yt-autoplay", "false");
+      ytShorts.loop = true;
+    }
+  });
+};
+
+const setProgressBar = (overlayList) => {
+  var progBarList = overlayList.children[2].children[0].children[0];
+  var progBarBG = progBarList.children[0];
+  var progBarPlayed = progBarList.children[1]; // The red part of the progress bar
+
+  const timestampTooltip = document.createElement("div");
+  timestampTooltip.classList.add("betterYT-timestamp-tooltip");
+
+  progBarList.appendChild(timestampTooltip);
+
+  // Styling to ensure rest of bottom overlay (shorts title/sub button) stay in place
+  overlayList.children[0].style.marginBottom = "-7px";
+  progBarList.style.height = "10px";
+  progBarList.style.paddingTop = "2px"; // Slight padding to increase hover box
+
+  progBarList.classList.add('betterYT-progress-bar');
+  progBarBG.classList.add('betterYT-progress-bar');
+  progBarPlayed.classList.add('betterYT-progress-bar');
+  return (progBarList, timestampTooltip);
+};
+
+const setProgressBarFuncs = (ytShorts, progBarList, timestampTooltip) => {
+  // Force progress bar to be visible for sub-30s shorts
+  if (ytShorts.duration < 30) progBarList.removeAttribute("hidden"); 
+
+  progBarList.addEventListener("mouseover", () => {
+    progBarBG.classList.add('betterYT-progress-bar-hover');
+    progBarPlayed.classList.add('betterYT-progress-bar-hover');
+  });
+  progBarList.addEventListener("mousemove", (event) => {
+    let x = event.clientX - progBarList.getBoundingClientRect().left;
+    // Deal with slight inaccuracies
+    if (x < 0) x = 0;
+    if (x > progBarList.clientWidth) x = progBarList.clientWidth;
+    // Get timestamp and round to nearest 0.1
+    let timestamp = ((x / progBarList.clientWidth) * ytShorts.duration).toFixed(1);
+    timestampTooltip.textContent = `${timestamp}s`;
+    // Ensure tooltip stays visible at edges of client
+    if ((x - (timestampTooltip.offsetWidth / 2)) > (progBarList.clientWidth - timestampTooltip.offsetWidth)) {
+      timestampTooltip.style.left = `${progBarList.clientWidth - timestampTooltip.offsetWidth}px`;
+    } else if ((x - (timestampTooltip.offsetWidth / 2)) <= 0) {
+      timestampTooltip.style.left = "0px";
+    } else {
+      timestampTooltip.style.left = `${x - (timestampTooltip.offsetWidth / 2)}px`;
+    }
+    timestampTooltip.style.top = "-20px";
+    timestampTooltip.style.display = 'block';
+  });
+  progBarList.addEventListener("mouseout", () => {
+    progBarBG.classList.remove('betterYT-progress-bar-hover');
+    progBarPlayed.classList.remove('betterYT-progress-bar-hover');
+    timestampTooltip.style.display = 'none';
+  });
+  progBarList.addEventListener("click", (event) => {
+    let x = event.clientX - progBarList.getBoundingClientRect().left;
+    if (x < 0) x = 0;
+    if (x > progBarList.clientWidth) x = progBarList.clientWidth;
+    ytShorts.currentTime = (x / progBarList.clientWidth) * ytShorts.duration;
+  });
 };
 
 const setVolumeSlider = (ytShorts, id) => {
@@ -163,23 +317,16 @@ const setPlaybackRate = (currSpeed) => {
   return true
 };
 
-var injectedItem = new Set();
-var lastTime = -1;
-var lastSpeed = 0;
-var setSpeed = 1;
-
 const timer = setInterval(() => {
   if (window.location.toString().indexOf("youtube.com/shorts/") < 0) return;
-  const ytShorts = document.querySelector(
-    "#shorts-player > div.html5-video-container > video"
-  );
+  const ytShorts = document.querySelector("#shorts-player > div.html5-video-container > video");
   var currentId = getCurrentId();
   var actionList = getActionElement(currentId);
   var overlayList = getOverlayElement(currentId);
   var autoplayEnabled = localStorage.getItem("yt-autoplay") === "true" ? true : false;
   if (autoplayEnabled === null) autoplayEnabled = false;
 
-  if (injectedItem.has(currentId)) {
+  if (injectedItems.has(currentId)) {
     var currTime = Math.round(ytShorts.currentTime);
     var currSpeed = ytShorts.playbackRate;
 
@@ -192,8 +339,11 @@ const timer = setInterval(() => {
       // Using this as a check whether the elements actually were injected on the page
       var injectedSuccess = setTimer(currTime, Math.round(ytShorts.duration || 0));
       // If failed, retry injection during next interval
-      if (!injectedSuccess) injectedItem.delete(currentId);
-      lastTime = currTime;
+      if (!injectedSuccess) {
+        injectedItems.delete(currentId) 
+      } else {
+        lastTime = currTime;
+      };
     }
     if (currSpeed != lastSpeed) {
       const setRateSuccess = setPlaybackRate(currSpeed);
@@ -206,148 +356,12 @@ const timer = setInterval(() => {
     if (autoplayEnabled && ytShorts) ytShorts.loop = false;
 
     if (actionList) {
-
-      const betterYTContainer = document.createElement("div");
-      betterYTContainer.id = "betterYT-container";
-      betterYTContainer.setAttribute("class", "button-container style-scope ytd-reel-player-overlay-renderer");
-
-      const ytdButtonRenderer = document.createElement("div");
-      ytdButtonRenderer.setAttribute("class", "betterYT-renderer style-scope ytd-reel-player-overlay-renderer");
-
-      const ytButtonShape = document.createElement("div");
-      ytButtonShape.setAttribute("class", "betterYT-button-shape");
-
-      const ytLabel = document.createElement("label");
-      ytLabel.setAttribute("class", "yt-spec-button-shape-with-label");
-
-      const ytButton = document.createElement("button");      
-      ytButton.setAttribute("class", "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--mono yt-spec-button-shape-next--size-l yt-spec-button-shape-next--icon-button ");
-      // Playback Rate
-      var para0 = document.createElement("p");
-      para0.classList.add("betterYT");
-      para0.id = `ytPlayback${currentId}`;
-
-      // Timer
-      const ytTimer = document.createElement("div");
-      ytTimer.classList.add("yt-spec-button-shape-with-label__label");
-      var span1 = document.createElement("span");
-      span1.setAttribute("class", "yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--text-alignment-center yt-core-attributed-string--word-wrapping");
-      span1.id = `ytTimer${currentId}`;
-      span1.setAttribute("role", "text");
-      ytTimer.appendChild(span1);
-
-      // Match YT's HTML structure
-      ytButton.appendChild(para0);
-      ytLabel.appendChild(ytButton);
-      ytLabel.appendChild(ytTimer);
-      ytButtonShape.appendChild(ytLabel);
-      ytdButtonRenderer.appendChild(ytButtonShape);
-      betterYTContainer.appendChild(ytdButtonRenderer);
-
-      actionList.insertBefore(betterYTContainer, actionList.children[1]);
-
-      // Autoplay Switch
-      const switchContainer = document.createElement("div");
-      const autoplaySwitch = document.createElement("label");
-      autoplaySwitch.classList.add("autoplay-switch");
-      var checkBox = document.createElement("input");
-      checkBox.type = "checkbox";
-      checkBox.id = `autoplay-checkbox${currentId}`;
-      checkBox.checked = autoplayEnabled;
-      var autoplaySpan = document.createElement("span");
-      autoplaySpan.classList.add("autoplay-slider");
-      autoplaySwitch.append(checkBox, autoplaySpan);
-      switchContainer.appendChild(autoplaySwitch);
-
-      actionList.insertBefore(switchContainer, actionList.children[1]);
-      
-      const autoplayTitle = document.createElement("div");
-      autoplayTitle.classList.add("yt-spec-button-shape-with-label__label");
-      var span2 = document.createElement("span");
-      span2.setAttribute("class", "betterYT-auto yt-core-attributed-string yt-core-attributed-string--white-space-pre-wrap yt-core-attributed-string--text-alignment-center");
-      span2.setAttribute("role", "text");
-      span2.textContent = "Autoplay";
-      autoplayTitle.appendChild(span2);
-    
-      actionList.insertBefore(autoplayTitle, actionList.children[2]);
-      injectedItem.add(currentId);
-
-      ytShorts.playbackRate = setSpeed;
-      setPlaybackRate(setSpeed);
-      injectedSuccess = setTimer(currTime || 0, Math.round(ytShorts.duration || 0));
-
-      betterYTContainer.addEventListener("click",() => {
-        ytShorts.playbackRate = 1;
-        setSpeed = ytShorts.playbackRate;
-      });
-
-      checkBox.addEventListener('change', () => {
-        if (checkBox.checked) {
-          localStorage.setItem("yt-autoplay", "true");
-          ytShorts.loop = false;
-        } else {
-          localStorage.setItem("yt-autoplay", "false");
-          ytShorts.loop = true;
-        }
-      });
+      injectedSuccess = setActionList(currentId, actionList);
+      setActionListFuncs(ytShorts);
     }
-    // Progress bar
     if (overlayList) {
-      var progBarList = overlayList.children[2].children[0].children[0];
-      var progBarBG = progBarList.children[0];
-      var progBarPlayed = progBarList.children[1]; // The red part of the progress bar
-
-      // Force progress bar to be visible for sub-30s shorts
-      if (ytShorts.duration < 30) progBarList.removeAttribute("hidden"); 
-
-      const timestampTooltip = document.createElement("div");
-      timestampTooltip.classList.add("betterYT-timestamp-tooltip");
-
-      progBarList.appendChild(timestampTooltip);
-
-      // Styling to ensure rest of bottom overlay (shorts title/sub button) stay in place
-      overlayList.children[0].style.marginBottom = "-7px";
-      progBarList.style.height = "10px";
-      progBarList.style.paddingTop = "2px"; // Slight padding to increase hover box
-
-      progBarList.classList.add('betterYT-progress-bar');
-      progBarBG.classList.add('betterYT-progress-bar');
-      progBarPlayed.classList.add('betterYT-progress-bar');
-
-      progBarList.addEventListener("mouseover", () => {
-        progBarBG.classList.add('betterYT-progress-bar-hover');
-        progBarPlayed.classList.add('betterYT-progress-bar-hover');
-      });
-      progBarList.addEventListener("mousemove", (event) => {
-        let x = event.clientX - progBarList.getBoundingClientRect().left;
-        // Deal with slight inaccuracies
-        if (x < 0) x = 0;
-        if (x > progBarList.clientWidth) x = progBarList.clientWidth;
-        // Get timestamp and round to nearest 0.1
-        let timestamp = ((x / progBarList.clientWidth) * ytShorts.duration).toFixed(1);
-        timestampTooltip.textContent = `${timestamp}s`;
-        // Ensure tooltip stays visible at edges of client
-        if ((x - (timestampTooltip.offsetWidth / 2)) > (progBarList.clientWidth - timestampTooltip.offsetWidth)) {
-          timestampTooltip.style.left = `${progBarList.clientWidth - timestampTooltip.offsetWidth}px`;
-        } else if ((x - (timestampTooltip.offsetWidth / 2)) <= 0) {
-          timestampTooltip.style.left = "0px";
-        } else {
-          timestampTooltip.style.left = `${x - (timestampTooltip.offsetWidth / 2)}px`;
-        }
-        timestampTooltip.style.top = "-20px";
-        timestampTooltip.style.display = 'block';
-      });
-      progBarList.addEventListener("mouseout", () => {
-        progBarBG.classList.remove('betterYT-progress-bar-hover');
-        progBarPlayed.classList.remove('betterYT-progress-bar-hover');
-        timestampTooltip.style.display = 'none';
-      });
-      progBarList.addEventListener("click", (event) => {
-        let x = event.clientX - progBarList.getBoundingClientRect().left;
-        if (x < 0) x = 0;
-        if (x > progBarList.clientWidth) x = progBarList.clientWidth;
-        ytShorts.currentTime = (x / progBarList.clientWidth) * ytShorts.duration;
-      });
+      let (progBarList, timestampTooltip) = setProgressBar(overlayList);
+      setProgressBarFuncs(ytShorts, progBarList, timestampTooltip);
     }
     if (currentId !== null) setVolumeSlider(ytShorts, currentId);
   }
